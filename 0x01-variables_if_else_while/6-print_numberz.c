8af059a6617423ea58bc885e16af0a3fdc252456
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-19 18:07:54 -0700

    new

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: whatever your description is
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

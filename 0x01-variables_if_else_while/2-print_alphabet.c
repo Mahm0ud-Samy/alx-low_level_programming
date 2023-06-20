e5317ef3bd6316cc908f1badc4b2b25b8c2a2162
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-19 17:15:46 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: whatever your description is
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}

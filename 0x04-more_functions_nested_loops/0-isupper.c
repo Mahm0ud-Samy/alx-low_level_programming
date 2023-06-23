b1954d9140cced4e011bb893030985634eb43062
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-23 11:40:54 -0700

    new

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,12 @@
 #include <stdbool.h>
 
+/**
+ *  main - check the code.
+ *
+ *  Description: print uppercase letters
+ *
+ *  Return: Always 0.
+ */
 bool _isupper(char c)
 {
 	return (c >= 'A' && c <= 'Z');

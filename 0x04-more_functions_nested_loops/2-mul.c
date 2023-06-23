0976a91c6e0df8ed13d598063862f7eaa11c27a1
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-23 14:31:16 -0700

    new

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * mul - multiply two integer numbers
+ *
+ * @a: input for integer number
+ *
+ * @b: input for integer number
+ *
+ * Return: the result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

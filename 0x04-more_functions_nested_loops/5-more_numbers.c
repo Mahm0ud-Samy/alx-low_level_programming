099b472cd741b9962f05937d38895625684c2301
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-23 15:42:02 -0700

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * more_numers - print numbers from 0 to 14 for 1o times
+ *
+ * Return: 1o times from 0 to 14 the numbers
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; i++)
+		{
+			_putchar('0' +j);
+		}
+	_putchar('\n');	
+        }
+}

3a6df74459904e558fc499c988017d10fbfd8910
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-23 12:03:31 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+/**
+ * main - entry point
+ *
+ * Description: whatever your description is
+ *
+ * Return: Always 0 (Succes).
+ */
+int _isdigit(int c)
+{
+	return (c >= 0 && c <= 9);
+}

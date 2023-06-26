ca7e828b3b16712d11ab0ca26cc973b42666436a
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-26 12:38:03 -0700

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - writes the charcter c to stdout
+ * @n: dereference operator & indirection operator
+ *
+ * Return: reset n to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

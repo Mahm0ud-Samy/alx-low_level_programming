735b06200635ce28681cc869826e80640ecaaa5e
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-26 19:49:22 -0700

    new

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_array - writes the charcter c to stdout
+ * @a: The character to print
+ * @n: num
+ *
+ * Return: On success 1.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf('\n');
+}

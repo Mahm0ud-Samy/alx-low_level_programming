5276c21eb4dcccb02b57cddf3dc273c6474f9747
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-26 19:33:21 -0700

    new

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+include <unistd.h>
+#include "main.h"
+
+/**
+ * puts - writes the charcter c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}

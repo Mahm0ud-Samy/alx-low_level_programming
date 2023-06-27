083f7e11988209f395734c2c7d0b5a5abe8bea9a
Mahm0ud-Samy <mahmoudengineer112003@gmail.com>
2023-06-26 18:49:56 -0700

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - display the whole string array
+ * @str: Array of characters
+ *
+ * Return: On success display the string.
+ */
+
+void _puts(char *str)
+{
+	while (str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n\);
+}
